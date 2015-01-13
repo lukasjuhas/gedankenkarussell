1afdaf16f348c20de5c5271ed52476ce24453467
Lukas Juhas <hello@lukasjuhas.com>
2015-01-13 14:30:41 +0000

    Email feedback.
    Changes, improvements

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,1 +1,10 @@
+jQuery(document).ready(function($) { 
+	$('.impressum').on('click', function(e) {
+		e.preventDefault();
+		$('.impressum-content').slideToggle();
 
+		 $('html, body').animate({
+	        scrollTop: $(".impressum-content").offset().top
+	    }, 2000);
+	});
+});
